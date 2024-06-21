baff8a9f1a36383025e900d980859b95e7749ba2
Kapil2508 <99133992+Kapil2508@users.noreply.github.com>
2024-06-21 19:28:07 +0530

    Add files via upload
    1. Hibernate and Spring integrated Employee Management System: 
    Seamlessly manages employee data with efficient CRUD operations.
    2. Intuitive interface for adding, updating, and deleting employee records, 
    enhancing administrative tasks.
    3. Leveraging Hibernate and Spring frameworks ensures robust performance 
    and scalability for effective employee management.
    Key skills: java, spring, hibernate, MYSQL.

---
+++ employeeapi.sql
@@ -0,0 +1,10 @@
+create database employee;
+use employee;
+
+show tables;
+desc employees;
+
+insert into employee(firstName,lastName,email)values("kapil","vasanthan","kapilvasanthan25@mail.com");
+insert into employee(firstName,lastName,email)values("karthi","kumar","karwj775@gmail.com");
+
+select * from employees;
